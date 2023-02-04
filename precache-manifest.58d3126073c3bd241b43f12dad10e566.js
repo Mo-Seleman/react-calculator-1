033c2dd33e1e36e20bbece8e2e0f7e5471aedd25
Mo-Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2023-02-04 13:30:46 +0000

    Updates

---
+++ precache-manifest.58d3126073c3bd241b43f12dad10e566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d4936af375ec32ecf8f",
+    "url": "/react-calculator-1/static/css/main.a16d7b91.chunk.css"
+  },
+  {
+    "revision": "7d4936af375ec32ecf8f",
+    "url": "/react-calculator-1/static/js/main.7d4936af.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/react-calculator-1/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "3c08d543a86a6a6a3f74",
+    "url": "/react-calculator-1/static/js/2.3c08d543.chunk.js"
+  },
+  {
+    "revision": "ffb2e1972a0e6a2fc021",
+    "url": "/react-calculator-1/static/js/runtime~main.ffb2e197.js"
+  },
+  {
+    "revision": "cdd6780caa9850b474ad204fdbc7abdc",
+    "url": "/react-calculator-1/index.html"
+  }
+];
